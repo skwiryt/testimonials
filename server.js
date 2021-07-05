d7c205f3f6407f303ce0124b1adcb477dd970cb8
skwiryt <mzbprobny@gmail.com>
2021-07-05 20:07:37 +0200

    Connect to Atlas mongo db

--- server.js
+++ server.js
@@ -57,7 +57,8 @@ app.use((req, res) => {
 });
 
 
-mongoose.connect('mongodb://localhost:27017/newWaveDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/newWaveDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://skwiryt:Qwinto123@cluster0.zkoct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
